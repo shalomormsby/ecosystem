8b7be56e441388eb98c7354dea844b2efde025d6
Shalom Ormsby <shalomormsby@gmail.com>
2025-12-28 13:59:05 -0800

    Fix
    Fix: Temporarily disable Header export and add STUDIO_URL to turbo config

--- design-system/atoms/index.ts
+++ design-system/atoms/index.ts
@@ -1,7 +1,7 @@
 export { Button } from './Button/Button';
 export { Card } from './Card/Card';
-export { Header } from './Header';
-export type { HeaderProps, NavLink } from './Header';
+// export { Header } from './Header';
+// export type { HeaderProps, NavLink } from './Header';
 export { Link } from './Link/Link';
 export * from './Motion';
 
