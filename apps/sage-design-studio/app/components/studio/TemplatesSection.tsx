4f4c791b86a469cecfd68701b081f7ca0e15bfe5
Shalom Ormsby <shalomormsby@gmail.com>
2026-01-06 12:58:08 -0800

    secondaryNav fix 6 (edge-to-edge glass effect)

--- apps/sage-design-studio/app/components/studio/TemplatesSection.tsx
+++ apps/sage-design-studio/app/components/studio/TemplatesSection.tsx
@@ -48,7 +48,7 @@ export function TemplatesSection({ breadcrumbs, activeItemId, onItemChange }: Te
       </div>
 
       {/* Template Navigation */}
-      <div className="sticky top-0 z-10 -mx-4 sm:-mx-6 lg:-mx-8 px-4 sm:px-6 lg:px-8 mb-4">
+      <div className="sticky top-0 z-10 -mx-4 sm:-mx-6 lg:-mx-8 mb-4">
         <TertiaryNav
           items={[
             { id: 'templates-overview', label: 'Overview' },
