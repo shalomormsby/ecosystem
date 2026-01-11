7cdfdeaa9210f7bc6bb6f97e4b5917a54bece947
Shalom Ormsby <shalomormsby@gmail.com>
2026-01-11 08:56:41 -0800

    Fix 8 (CC)

--- packages/ui/src/components/Form.tsx
+++ packages/ui/src/components/Form.tsx
@@ -15,7 +15,7 @@ import {
 import { cn } from "../lib/utils"
 import { Label } from "./Label"
 
-const Form: typeof FormProvider = FormProvider
+const Form = FormProvider
 
 type FormFieldContextValue<
   TFieldValues extends FieldValues = FieldValues,
